8ca87b54a1cea277af2bdb38e4da16d8fa04a4e6
xTecciz <107061272+xTecciz@users.noreply.github.com>
2022-08-21 14:44:58 +0300

    update nav

--- app/src/main/java/com/tck/musicplayer/MainActivity.kt
+++ app/src/main/java/com/tck/musicplayer/MainActivity.kt
@@ -3,21 +3,21 @@ package com.tck.musicplayer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.tck.musicplayer.navigation.SetupNavGraph
 import com.tck.musicplayer.ui.theme.MusicPlayerTheme
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var navHostController: NavHostController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MusicPlayerTheme {
-
+                navHostController = rememberNavController()
+                SetupNavGraph(navHostController = navHostController)
             }
         }
     }
--- app/src/main/java/com/tck/musicplayer/navigation/Screen.kt
+++ app/src/main/java/com/tck/musicplayer/navigation/Screen.kt
@@ -1,7 +1,7 @@
 package com.tck.musicplayer.navigation
 
 sealed class Screen(val route: String) {
-    object SplashScreen:Screen("SplashScreen")
+    object SplashScreen : Screen("SplashScreen")
     object ScreenOne : Screen("ScreenOne")
     object ScreenTwo : Screen("ScreenTwo")
 }
