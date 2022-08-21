b0f0c121dba3953c898f334187ea4d9554c8aa3f
xTecciz <107061272+xTecciz@users.noreply.github.com>
2022-08-21 14:25:09 +0300

    Initial commit

---
+++ app/src/main/java/com/tck/musicplayer/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.tck.musicplayer
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.tck.musicplayer.ui.theme.MusicPlayerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MusicPlayerTheme {
+
+            }
+        }
+    }
+}
